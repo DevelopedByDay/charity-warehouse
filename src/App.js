9c5939bdc889f5ddc46ad59a5b1b42bd3924c040
durganp1 <durganp1@gmail.com>
2020-11-01 08:52:57 -0600

    working on the nave links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Nav from './components/Nav';
 import './App.css';
-import AnimalList from './components/animal';
-import ReligionList from './components/religion';
-import CommDevList from './components/community';
+
+import SingleCategory from './components/SingleCategory';
 
 
 function App() {
+
+  const [categories] = useState([
+    { name: 'animals'},
+    { name: 'arts'},
+    { name: 'civil-rights'},
+    { name: 'community'},
+    { name: 'environmental'},
+    { name: 'religion'}
+  ]);
+  
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+      categories = {categories}
+      setCurrentCategory = {setCurrentCategory}
+      currentCategory = {currentCategory}
+      ></Nav>
       <main>
-        <AnimalList></AnimalList>
+        <SingleCategory currentCategory = {currentCategory}></SingleCategory>
       </main>
 
     </div>
