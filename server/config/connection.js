adda86fd7bce8636f68f46e15ab719468af7d3f8
durganp1 <durganp1@gmail.com>
2020-11-07 23:49:29 -0600

    working on data base

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/charity-warehouse', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/charityWarehouse', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
